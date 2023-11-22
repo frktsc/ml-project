2203b8e05ecbde55a41064c213984150455fc1c7
frktsc <ftasc40@gmail.com>
2023-11-21 23:21:34 -0500

    setup dosyalari eklendi

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+hypen_e_dot = '-e .'
+
+
+
+def get_requirements(file_path:str)->List[str]:
+    #this def will return the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if hypen_e_dot in  requirements:
+            requirements.remove(hypen_e_dot)
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='furkan',
+    author_email='ftasc40@gmail.com',
+    packages=find_packages(),
+    requires=get_requirements('requirements.txt')
+    
+)
