64ea534a6e7999bbef60cdec73a96b27b6ee173a
frktsc <ftasc40@gmail.com>
2023-11-23 18:20:24 -0500

    flask

--- src/utils.py
+++ src/utils.py
@@ -62,3 +62,11 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
